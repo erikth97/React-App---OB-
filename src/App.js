5750ee74b340c14e77c22a24aa4fe27603dcf118
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-01 12:08:57 -0600

    primercommit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Greeting from './components/pure/greeting';
 import Greetingf from './components/pure/greetingF';
+import TaskListComponent from './components/container/task_list';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          {/* Componente propio Greeting.jsx */}
+          {/* Componente propio Greeting.jsx      */}
           {/* <Greeting name={"Erik"}></Greeting> */}
-          <Greetingf name="Erik"></Greetingf>
+          {/* Componente de ejemplo funcional  */}           
+          {/*<Greetingf name="Erik"></Greetingf>  */} 
+          {/* Componente de listado de Tareas     */}           
+          <TaskListComponent></TaskListComponent>
       </header>
     </div>
   );
