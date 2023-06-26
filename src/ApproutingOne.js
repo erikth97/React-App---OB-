286480c537c9d14778f9b3834a9d7a38ebfc76c9
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-26 11:33:49 -0600

    routes-example

---
+++ src/ApproutingOne.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function AppRoutingOne() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default AppRoutingOne;
