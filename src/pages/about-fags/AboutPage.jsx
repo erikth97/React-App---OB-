0f9fa1a71e9e9bcfd7e1720a7baf41ceb8fee35b
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-26 14:29:21 -0600

    aboutPage

---
+++ src/pages/about-fags/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>
+                About | FAQs Page
+            </h1>
+        </div>
+    );
+}
+
+export default AboutPage;
