ba299535039f1d1ef10cc44c5337606245e9bf46
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-26 14:30:08 -0600

    AppRouting

--- src/ApproutingOne.js
+++ src/ApproutingOne.js
@@ -1,10 +1,19 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import HomePage from './pages/home/HomePage';
+import NotFoundPage from './pages/404/NotFoundPage';
+import AboutPage from "./pages/about-fags/AboutPage";
+
 function AppRoutingOne() {
   return (
-    <div className="App">
-
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" Component={ HomePage} />
+          <Route path='/(about|faqs)' component={ AboutPage } />
+          {/* 404 - page not found*/}
+          <Route Component={ NotFoundPage } />
+        </Switch>
+      </Router>
   );
 }
 
