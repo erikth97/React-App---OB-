8848e45499dc4d28e95da43c69545425de943798
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-05 08:18:59 -0600

    statePage

---
+++ src/pages/home/StatePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const StatePage = () => {
+    const Statepage = () =>{
+
+    return (
+        <div>
+            
+        </div>
+    
+    );
+}
+
+export default StatePage;
