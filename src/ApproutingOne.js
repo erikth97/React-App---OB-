f163cb97f11dd1536e8bd7ae4375c61000fa4774
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-05 08:20:52 -0600

    RoutingApp

--- src/ApproutingOne.js
+++ src/ApproutingOne.js
@@ -1,20 +1,35 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import HomePage from './pages/home/HomePage';
 import NotFoundPage from './pages/404/NotFoundPage';
 import AboutPage from "./pages/about-fags/AboutPage";
 
-function AppRoutingOne() {
+function ApproutingOne() {
   return (
       <Router>
-        <Switch>
-          <Route exact path="/" Component={ HomePage} />
-          <Route path='/(about|faqs)' component={ AboutPage } />
-          {/* 404 - page not found*/}
-          <Route Component={ NotFoundPage } />
-        </Switch>
+       <div>
+          <aside>
+            <Link to='/'>|| Home |</Link>
+            <Link to='/about'>| ABOUT |</Link>
+            <Link to='/faqs'>| FAQs |</Link>
+            <link to='/any404'>| Not Existing Route ||</link>
+          </aside>
+       
+          <main>
+          <Switch>
+            <Route exact path="/" Component={ HomePage } />
+            <Route path='/(about|faqs)' component={ AboutPage } />
+            {/* 404 - page not found*/}
+            <Route Component={ NotFoundPage } />
+          </Switch>
+          </main>
+       
+       </div>
+
+
+
       </Router>
   );
 }
 
-export default AppRoutingOne;
+export default ApproutingOne;
