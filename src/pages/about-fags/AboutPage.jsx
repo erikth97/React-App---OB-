560cafab64e719ac78e0c1b5ae8227184c9bae4e
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-05 08:19:26 -0600

    AboutPage

--- src/pages/about-fags/AboutPage.jsx
+++ src/pages/about-fags/AboutPage.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 
-const AboutPage = () => {
+import { useLocation, useHistory } from 'react-router-dom';
+
+const Aboutpage = () => {
+
+    const location = useLocation();
+    const history = useHistory();
+
+    console.log('We are in Route:', location.pathname); // '/about | /faqs'
+
+    const navigate = (path) => {
+        history.push(path);
+    }
+
+    const goBack = () => {
+        history.goBack();
+    }
+
+    const goForward = () => {
+        history.goForward()
+    }
+
     return (
         <div>
             <h1>
-                About | FAQs Page
+                About | FAQs
             </h1>
+            <div>
+                <button onClick={ () => navigate('/')}>
+                    Go To Home
+                </button>
+                <button onClick = { goBack }>
+                    Go Back
+                </button>
+                <button onClick = { goForward }>
+                    Go Forward
+                </button>
+            </div>
         </div>
     );
 }
 
-export default AboutPage;
+export default Aboutpage;
