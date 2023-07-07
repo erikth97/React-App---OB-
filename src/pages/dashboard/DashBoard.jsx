e190f06194838d43ddbcde27f1bc2181b08eeaa3
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-07 11:51:07 -0600

    ProjectFinal

---
+++ src/pages/dashboard/DashBoard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+import  Copyright  from '@mui/icons-material';
+
+const Dashboardpage = () => {
+    return (
+        <div>
+              <Button variant="contained">Hello World</Button>
+              <Copyright></Copyright>
+        </div>
+    );
+}
+
+export default Dashboardpage;
