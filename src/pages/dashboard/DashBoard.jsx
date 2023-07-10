c060ff5f20bbe36dbf5219d36594bcbfcd85d82b
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-10 12:07:50 -0600

    Dashboard

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom'
+
 import Button from '@mui/material/Button';
 
-import  Copyright  from '@mui/icons-material';
+import  Copyright  from '../../components/pure/Copyrigth';
 
 const Dashboardpage = () => {
+
+   const history = useHistory();
+
+   const logout = () => {
+      history.push('/login')
+   }
+
     return (
         <div>
-              <Button variant="contained">Hello World</Button>
+              <Button variant="contained" onClick={logout}>Logout</Button>
               <Copyright></Copyright>
         </div>
     );
