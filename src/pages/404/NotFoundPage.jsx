9b42c2bc98980cefa4f1697b75eb4d79925cb12d
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-04 14:48:56 -0600

    routing

--- src/pages/404/NotFoundPage.jsx
+++ src/pages/404/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-    const Notfoundpage = () => {
+const Notfoundpage = () => {
         
         const history = useHistory();
 
