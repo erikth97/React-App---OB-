a342318a88ea615bc33b55b4fdb305ef113f6889
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-09 12:29:21 -0600

    routingOne

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router';
 
 
-const HomePage = () => {
+const Homepage = () => {
     const location = useLocation();
     const history = useHistory();
 
-    console.log('We are in Router:', location.pathname); // '/about | /faqs'
+    console.log('We are in Route:', location.pathname); // '/about | /faqs'
 
     const navigate = (path) => {
         history.push(path);
@@ -15,7 +15,7 @@ const HomePage = () => {
     const navigateProps = (path) => {
         history.push({
             pathname: path,
-            search: '?onLine=true', // Query Params
+            search: '?online=true', // Query Params
             state: {
                 online: true
             }
@@ -23,15 +23,16 @@ const HomePage = () => {
     }
 
     return (
-    <div>
-        <h1>Home Page</h1>
-        <button onClick={() => navigateProps('/online-state')}>
-            Go to Page with State / Query Params   
-        </button>
-        <button onClick={() => navigate('/profile')}>
-            Go to Profile
-        </button>
-    </div>
-    )
+        <div>
+            <h1>Home Page</h1>
+            <button onClick={() => navigateProps('/online-state')}>
+                Go To Page with State / Query Params
+            </button>
+            <button onClick={() => navigate('/profile')}>
+                Go To Profile
+            </button>
+        </div>
+    );
 }
-export default HomePage;
+
+export default Homepage;
