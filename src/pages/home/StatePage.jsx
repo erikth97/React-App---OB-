1bc34052f8ed15e899bec643971d778add6e08a6
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-09 12:29:17 -0600

    routingOne

--- src/pages/home/StatePage.jsx
+++ src/pages/home/StatePage.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
-    const Statepage = () =>{
-        
-        const location = useLocation();
-        
-        console.log('Query Params:', location.search); // Query Params Sent
-        
-        return (
+const Statepage = () => {
+
+    const location = useLocation();
+
+    console.log('Query Params:', location.search); // Query Params Sent
+
+    return (
         <div>
-            <h1>State: {location.state.onLine ? 'The user is Online' : 'The user is Offline'}</h1>
+            <h1>State: {location.state.online ? 'The user is Online' : 'The user is Offline'}</h1>
         </div>
     );
 }
