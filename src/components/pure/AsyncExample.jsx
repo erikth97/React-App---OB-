d6bb1e9953f4e6de00263651a5f05822ce1a7905
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-10 14:56:56 -0600

    Async

---
+++ src/components/pure/AsyncExample.jsx
@@ -0,0 +1,85 @@
+
+import React from 'react';
+
+const AsyncExample = () => {
+
+   async function generateNumber(){
+    return 1;
+   }
+
+   async function generatePromiseNumber(){
+    return Promise.resolve(2)
+   }
+
+   function obtainNumber(){
+    generateNumber()
+           .then((response) => alert(`Response: ${response}`))
+           .catch((error) => alert(`Something went wrong: ${error}`))
+   }
+
+   function obtainPromiseNumber(){
+    generatePromiseNumber()
+           .then((response) => alert(`Response: ${response}`))
+           .catch((error) => alert(`Something went wrong: ${error}`))
+   }
+
+   async function saveSesionStorage(key, value) {
+      sessionStorage.setItem(key, value);
+       return Promise.resolve(sessionStorage.getItem(key))
+   }
+
+   function showStorage(){
+        saveSesionStorage('name', 'Erik')
+           .then((response) => {
+            let value = response;
+            alert(`Saved Name: ${value}`);
+           }).catch((error) => {
+            alert(`Something went wrong: ${error}`)
+           }).finally(() => { 
+           alert('SUCCESS: Name saved and retreived')
+        })
+   }
+
+   async function obtainMessage() {
+
+       let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('Hello Word'), 2000)
+       });
+
+       let message = await promise;
+       await alert(`Message received: ${message}`)
+   
+    }
+
+    const consumeError = async() => {
+        returnError()
+           .then((response) =>alert(`Everything is OK: ${response}`))
+           .catch((error) => alert(`Something went wrong: ${error}`))
+           .finally(() => alert('Finally executed'))            
+    }
+
+    const urlNotFound = async () => {
+        try {
+            let response = await fetch('https://invalidURL.com')
+            alert(`Response: ${JSON.stringify(response)}`)      
+        } catch (error) {
+            alert(`Something went wrong whith your URL: ${error} (check your console)`)
+        }
+
+    }
+
+
+    return (
+        <div>
+             <h1>Async, Promise Examples</h1>
+            <button onClick={obtainNumber}>Obtain Number</button>
+            <button onClick={obtainPromiseNumber}>Obtain Promise Number</button>  
+            <button onClick={showStorage}>Save Name and Show</button>    
+            <button onClick={obtainMessage}>Recive message in 2 seconds</button>    
+            <button onClick={consumeError}>Obtain Error</button>  
+            <button onClick={urlNotFound}>Request to Unknow URL</button>  
+        </div>
+    );
+}
+
+export default AsyncExample;
