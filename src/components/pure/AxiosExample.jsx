84e6f9197ec00619d1b602732283b13a24e233ec
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-14 15:10:26 -0600

    axiosExample

--- src/components/pure/AxiosExample.jsx
+++ src/components/pure/AxiosExample.jsx
@@ -1,26 +1,48 @@
 import React, {useState, useEffect} from 'react';
 import { getRandomUser } from '../../services/axiosService';
 
-const AxiosExample = () => {
+const Axiosexample = () => {
 
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState(null);
 
-    useEffect(() => {
+    //useEffect(() => {
+    //obtainUser();
+    //}, []);       
+
+    const obtainUser = () => {
         getRandomUser()
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                alert(`Something went wrong: ${error}`);
-            })
-        }, [])       
+        .then((response) => {
+            if(response.status === 200) {
+                setUser(response.data.results)
+            }
+        })
+        .catch((error) => {
+            alert(`Something went wrong: ${error}`);
+        }) 
+    }
 
-    
     return (
         <div>
-
+           <h1>Axios</h1>
+           { user != null ? 
+                (
+                    <div>
+                        {/*<img src={user.picture.large} alt="avatar" />*/}
+                        <h2>{user.name.title} {user.name.frist} {user.name.last}</h2>
+                        <h3>{user.email}</h3>
+                    </div>
+                )
+            :
+            ( 
+                <div>
+                    <p>Generate a new User</p>
+                    <button onClick={obtainUser}>
+                        Random User
+                    </button>
+                </div>
+            )}   
         </div>
     );
 }     
 
-export default AxiosExample;
+export default Axiosexample;
