f75872d218c0512590e0d4e01d8bd76f3f5c2bf7
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-08-15 13:59:29 -0600

    axiosCrud

--- src/components/pure/AxiosCRUDExample.jsx
+++ src/components/pure/AxiosCRUDExample.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-import { login } from '../../services/axiosCRUDService';
+import { login, getAllUsers, getAllPagedUsers, getUserByID, createUser, updateUserByID, deleteUserByID } from '../../services/axiosCRUDService';
 
 const Axioscrudexample = () => {
 
@@ -38,6 +38,80 @@ const Axioscrudexample = () => {
                 })
                 .finally(() => console.log('Login donde'))
         } 
+
+        // CRUD EXAMPLES
+        const obtainAllUsers = () => {
+            getAllUsers()
+                .then((response) => {
+                    if(response.data.data && response.status === 200){
+                        alert(JSON.stringify(response.data.data))
+                    }else {
+                        throw new Error(`No Users found`)
+                    }
+                })
+                .catch((error) => alert(`Something went wrong: ${error}`))
+        }
+
+        const obtainAllPagedUsers = (page) => {
+            getAllPagedUsers(page)
+                .then((response) => {
+                    if(response.data.data && response.status === 200){
+                        alert(JSON.stringify(response.data.data))
+                    }else {
+                        throw new Error(`No Users found in page ${page}`)
+                    }
+                })
+                .catch((error) => alert(`Something went wrong: ${error}`))
+        }
+
+        const obtainUserByID = (id) => {
+             getUserByID(id)
+                .then((response) => {
+                    if(response.data.data && response.status === 200){
+                        alert(JSON.stringify(response.data.data))
+                    }else {
+                        throw new Error(`User not found`)
+                    }
+                })
+                .catch((error) => alert(`Something went wrong: ${error}`))
+        }
+
+        const createNewUser = (name, job) => {
+              createUser(name, job)                 
+              .then((response) => {
+                if(response.data && response.status === 201){
+                    alert(JSON.stringify(response.data))
+                }else {
+                    throw new Error(`User not created`)
+                }
+            })
+            .catch((error) => alert(`Something went wrong: ${error}`))                       
+        }
+
+        const updateUser = (id, name, job) => {
+            updateUserByID(id, name, job)
+               .then((response) => {
+                   if(response.data && response.status === 200){
+                       alert(JSON.stringify(response.data))
+                   }else {
+                       throw new Error(`User not found & no update donde`)
+                   }
+               })
+               .catch((error) => alert(`Something went wrong: ${error}`))
+       }
+
+       const deleteUser = (id) => {
+        deleteUserByID(id)
+           .then((response) => {
+               if(response.status === 204){
+                   alert(`User with id: ${id} successfully deleted`)
+               }else {
+                   throw new Error(`User not found & no delete done`)
+               }
+           })
+           .catch((error) => alert(`Something went wrong: ${error}`))
+   }
+    
     
         return (
             <div>
@@ -90,9 +164,28 @@ const Axioscrudexample = () => {
                     {isSubmitting ? (<p>Login your credentials...</p>): null}
                </Form>
             )}
-
             </Formik>
-
+            {/* Example buttons to test API responses*/}
+            <div>
+                <button onClick={obtainAllUsers}>
+                    Get All Users with Axios
+                </button>
+                <button onClick={() => obtainAllPagedUsers(1)}>
+                    Get All Users (Page 1) with Axios
+                </button>
+                <button onClick={() => obtainUserByID(1)}>
+                    Get User 1
+                </button>
+                <button onClick={() => createNewUser('morpheus', 'leader')}>
+                    Create User
+                </button>
+                <button onClick={() => updateUser(1, 'morpheus', 'Developer')}>
+                    Update User
+                </button>
+                <button onClick={() => deleteUser(1)}>
+                    Delete User
+                </button>
+            </div>
         </div>
         );
 }
